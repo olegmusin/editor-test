86ad8f8b705dac49b29662b5bdf0cc61511d47d4
Oleg Musin <oleg@pulseid.com>
2018-09-26 20:34:53 +0300

    small styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,70 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './main.css';
 import './bootstrap.css';
 
-class App extends Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleUploadImage = this.handleUploadImage.bind(this);
+    this.handleUploadText = this.handleUploadText.bind(this);
+  }
+
+  handleUploadImage() {
+    console.log('#imageUpload', !!this);
+  }
+
+  handleUploadText() {
+    console.log('#textUpload', !!this);
+  }
+
   render() {
     return (
       <div>
-        {/* <!-- side pane --> */}
-        <div className="sidepane col-sm-2 col-md-2 col-lg-2">
-          <div className="form">
-            <h3>Form</h3>
-            <input
-              type="file"
-              className="form-control"
-              placeholder="Upload Your Images"
-              name="upload"
-            />
-            <button id="submit" className="btn btn-default">upload</button>
-            {/* <!-- Upload Form here --> */}
+        <div className="sidepane col-sm-4 col-md-4 col-lg-4">
+          <div>
+            <h3>Tools</h3>
+            <hr />
+            <div className="form-group">
+              <input
+                type="file"
+                name="file"
+                id="file"
+                className="form-control"
+              />
+              <button onClick={this.handleUploadImage} className="btn btn-info">
+                Upload Image
+              </button>
+            </div>
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Input text to add..."
+              />
+              <button onClick={this.handleUploadText} className="btn btn-info">
+                Add Text
+              </button>
+            </div>
           </div>
-          <div className="assets">
+          <form>
             <h3>Assets</h3>
+            <hr />
             <div className="text">
               <h4>Text</h4>
-              <button id="addText" className="btn btn-default">Add Text</button>
+              <div className="well">...</div>
             </div>
             <div className="image">
               <h4>Images</h4>
-              <ul className="list-unstyled">
-                {/* <!-- List of images here --> */}
-                {/* <!-- <li><img src="images/sample.jpeg" class="img-rounded" /></li> --> */}
-              </ul>
+              <div className="well">
+                <ul className="list-unstyled">
+                  {/* <!-- List of images here --> */}
+                  {/* <!-- <li><img src="images/sample.jpeg" class="img-rounded" /></li> --> */}
+                </ul>
+              </div>
             </div>
-          </div>
+          </form>
         </div>
         {/* <!-- canvas --> */}
         <div className="canvas col-sm-8 col-md-8 col-lg-8">
